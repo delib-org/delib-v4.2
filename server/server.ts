6cbdb63b543ce7a2efc501cd0710e0107e94e25e
Tal Yaron <tal.yaron@gmail.com>
2022-06-17 16:52:57 +0300

    srg

--- server/server.ts
+++ server/server.ts
@@ -11,14 +11,14 @@ import { userConnect } from "./controls/socket";
 
 import Decision from 'models/m/decisionModel'
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static('client/build'))
 
