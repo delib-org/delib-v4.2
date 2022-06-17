07bfec616e195eadefe8effd16da2811c148093e
Tal Yaron <tal.yaron@gmail.com>
2022-06-17 10:03:12 +0300

    clean logs

--- Controls/socket.ts
+++ Controls/socket.ts
@@ -5,14 +5,13 @@ export async function userConnect(socket: any) {
   console.log(socket.id);
   socket.on("join-room", (roomId: any) => {
     console.log("join room ", roomId);
-   
+
     if (roomId) {
       socket.join(roomId);
       console.log(socket.rooms);
     }
   });
   socket.on("leave-room", (roomId: any) => {
-    console.log("leave room ", roomId);
     if (roomId) {
       socket.leave(roomId);
     }
@@ -20,14 +19,11 @@ export async function userConnect(socket: any) {
 
   socket.on("talk-to-room", ({ roomId, text }) => {
     try {
-      console.log(roomId, text);
-      console.log(roomId, "room-talk", text)
       io.to(roomId).emit("room-talk", text);
     } catch (error) {}
   });
 
   socket.on("msg", ({ text }) => {
-    console.log(text);
     io.emit("msg", { text });
   });
 
