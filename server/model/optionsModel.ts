c9a32c78f0b74e5eb24ae20f7e409124bbeca85b
Tal <tal.yaron@gmail.com>
2022-06-20 05:47:28 +0300

    all props bases are ready

--- server/model/optionsModel.ts
+++ server/model/optionsModel.ts
@@ -1,22 +1,29 @@
 import { uid } from "../controls/helpers/general";
 
-class Option{
-    id:string;
-    name:string;
-    description:string;
-    preferencesAggragation:Array<PreferencesAggragation> = [];
+class Option {
+    id: string;
+    name: string;
+    description: string;
+    preferencesAggragation: Array<PreferencesAggragation> = [];
 
-    constructor( name:string, description:string, id?:string){
-        this.id = id || uid();
-        this.name = name;
-        this.description = description;
+    constructor(name: string, description: string, id?: string) {
+        try {
+            this.id = id || uid();
+            this.name = name;
+            this.description = description;
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 
 }
 
-interface PreferencesAggragation{
-    type:string,
-    value:number
+
+
+interface PreferencesAggragation {
+    type: string,
+    value: number
 }
 
 export default Option;
---
+++ server/model/userModel.ts
@@ -0,0 +1,19 @@
+import { uid } from "../Controls/helpers/general";
+
+class User {
+    public id: number;
+    public name: string;
+    public email: string;
+    public created_at: Date;
+    public updated_at: Date;
+    
+    constructor ({id, name, email}) {
+        this.id = id || uid();
+        this.name = name;
+        this.email = email;
+        this.created_at = new Date();
+        this.updated_at = new Date();
+    }
+}
+
+export default User;
