936da12ed4e0b4a2457546d5cc96b7aac97eaf1c
Tal Yaron <tal.yaron@gmail.com>
2022-06-30 10:01:05 +0300

    coint

--- client/src/view/pages/consultations/subPages/Intro.tsx
+++ client/src/view/pages/consultations/subPages/Intro.tsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../../control/hooks";
+import { createEditor,BaseEditor, Descendant} from 'slate';
+import { Slate, Editable, withReact,ReactEditor } from 'slate-react';
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue:Array<any> = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
 
 const Intro = () => {
   const { consultationId } = useParams();
+  const [editor] = useState(() => withReact(createEditor()))
+
   const consultation = useAppSelector((state) =>
     state.consultations.consultations.find(
       (cnsl) => cnsl._id === consultationId
     )
   );
+
+
+
   if (consultation)
     return (
       <main>
         <div className="wrapper">
+        <Slate editor={editor} value={initialValue}>
+        <Editable />
+        </Slate>
           <div className="intro">{consultation.description}</div>
         </div>
       </main>
